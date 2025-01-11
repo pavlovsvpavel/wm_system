bc29c867707b39362bdc4270e1f243fd7d5df247
Pavel Pavlov <pavelsvpavlov@gmail.com>
2025-01-11 14:53:29 +0200

    Changes in docker-compose files.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Collect static files
 echo "Collect static files"
 python manage.py collectstatic --no-input
 
-# Start server
-#echo "Starting server"
-## python3 manage.py runserver 0.0.0.0:8000
-#gunicorn core.wsgi:application --bind=0.0.0.0:8000
+echo "Migrate database"
+python manage.py migrate
 
 exec "$@"
