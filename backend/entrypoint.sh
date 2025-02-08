9bb085cd13f2deb0ac62f77c85e69591c244e2e2
Pavel Pavlov <pavelsvpavlov@gmail.com>
2025-02-08 10:17:47 +0200

    Added github action

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,9 @@
 echo "Collect static files"
 python manage.py collectstatic --no-input
 
+echo "Make migrations to database"
+python manage.py makemigrations
+
 echo "Migrate database"
 python manage.py migrate
 
